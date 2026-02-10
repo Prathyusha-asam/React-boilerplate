05d2839c3804aa6480daae9ac33f69bb97729749
Asam-Prathyusha <asam.p@saksoft.com>
2026-02-10 17:01:01 +0530

    made changes

---
+++ src/pages/ProductDetails.jsx
@@ -0,0 +1,16 @@
+import { useParams } from 'react-router-dom'
+
+function ProductDetails() {
+  const { productId } = useParams()
+
+  return (
+    <div className="page">
+      <h1>Product Details</h1>
+      <p>You are viewing details for product ID: {productId}</p>
+    </div>
+  )
+}
+
+export default ProductDetails
+
+
